185487f69dd9d5c416090b5070c02c82f326c143
cesar <cesiha13@gmail.com>
2024-04-08 20:22:19 -0300

    Alterações na app.dart para incluir outras telas

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,60 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SplashPage(),
+      title: 'Delivery App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/pedidos': (context) => PedidosScreen(),
+        '/login': (context) => LoginScreen(),
+        '/perfil': (context) =>PerfilScreen(),
+      },
+    );
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_cart),
+          label: 'Pedidos',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Perfil',
+        ),
+      ],
+      currentIndex: 0, // Índice inicial
+      selectedItemColor: Colors.blue, // Cor do item selecionado
+      onTap: (int index) {
+        // Função chamada quando um item é pressionado
+        if (index == 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+          );
+        } else if (index == 1) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PedidosScreen()),
+          );
+        } else if (index == 2) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PerfilScreen()),
+          );
+        }
+      },
     );
   }
 }
